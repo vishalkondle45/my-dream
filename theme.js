6ecee136bea39d241c3ef86c6a0bb7265828ad7b
Vishal <vishal.kondle@gmail.com>
2023-10-21 20:00:15 +0530

    fix: removed unwanted styles from theme

--- theme.js
+++ theme.js
@@ -6,15 +6,8 @@ export const theme = createTheme({
   fontFamily: "Inter, Verdana, sans-serif",
   headings: { fontFamily: "Inter, Verdana, sans-serif" },
   primaryColor: "dark",
-  primaryShade: {
-    dark: 9,
-    light: 5,
-  },
-  components: {
-    TextInput: TextInput.extend({
-      defaultProps: {
-        color: "white",
-      },
-    }),
-  },
+  // primaryShade: {
+  //   dark: 9,
+  //   light: 5,
+  // },
 });
